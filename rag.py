c812418a73fcf161518a54881cb758b4c7a82461
Shreya Gupta <40699385+ShreyaGupta08@users.noreply.github.com>
2023-10-13 14:14:53 -0700

    Update rag.py

--- rag.py
+++ rag.py
@@ -4,7 +4,7 @@ llm = RetrievalAugmentedRunner(
     model_name = "meta-llama/Llama-2-13b-chat-hf",
 )
 
-llm.load_data("data")
+llm.load_data("data/")
 llm.train()
 response = llm("Who won the case above about dana and wells fargo?")
 print("\nResponse: ", response)
